6dd534ca87011626155b864b500f88166fc13050
thang241190 <thangnguyen24111990@gmail.com>
2021-03-05 23:23:02 +0200

    Testing

--- src/components/company/CompanyComponent.tsx
+++ src/components/company/CompanyComponent.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react"
+import React, { useState } from "react"
 import { CountUp } from "use-count-up"
 
 import "./company.scss"
@@ -9,24 +9,24 @@ import contact from "./images/contact.jpg"
 
 const CompanyComponent: React.FC = () => {
 	const [isCounting, setIsCounting] = useState(false)
-	let scrollY: number = 0
-	function scroll() {
-		const companySection = document.querySelector(".company") as HTMLElement
-		scrollY = window.pageYOffset
-		if (scrollY > companySection.getBoundingClientRect().top) {
-			setIsCounting(true)
-		}
-	}
+	// let scrollY: number = 0
+	// function scroll() {
+	// 	const companySection = document.querySelector(".company") as HTMLElement
+	// 	scrollY = window.pageYOffset
+	// 	if (scrollY > companySection.getBoundingClientRect().top) {
+	// 		setIsCounting(true)
+	// 	}
+	// }
 
-	useEffect(() => {
-		function watchScroll() {
-			window.addEventListener("scroll", scroll)
-		}
-		watchScroll()
-		return () => {
-			window.removeEventListener("scroll", scroll)
-		}
-	}, [])
+	// useEffect(() => {
+	// 	function watchScroll() {
+	// 		window.addEventListener("scroll", scroll)
+	// 	}
+	// 	watchScroll()
+	// 	return () => {
+	// 		window.removeEventListener("scroll", scroll)
+	// 	}
+	// }, [])
 	return (
 		<div className='company'>
 			<div className='container'>
