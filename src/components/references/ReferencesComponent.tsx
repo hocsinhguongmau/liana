c44c47273bbb904e6a29b96888bda22c849d3f8f
thang241190 <thangnguyen24111990@gmail.com>
2021-03-07 16:16:10 +0200

    Testing

--- src/components/references/ReferencesComponent.tsx
+++ src/components/references/ReferencesComponent.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import Slider from "react-slick"
+// import Slider from "react-slick"
 import { useTranslation } from "react-i18next"
 import "../../i18n"
 
@@ -34,66 +34,66 @@ const ReferencesComponent: React.FC = () => {
 			<div className='container'>
 				<h3 className='section-title'>{t("title.references")}</h3>
 				<div className='reference-items'>
-					<Slider {...settings}>
-						<div className='col'>
-							<div className='reference-item'>
-								<div className='reference-img'>
-									<img src={ref1} alt='Bosch' />
-								</div>
-								<div className='reference-text'>
-									<p className='reference-text-title'>
-										Robert Bosch
-									</p>
-									<p className='reference-text-description'>
-										{t("reference.bosch")}
-									</p>
-								</div>
+					{/* <Slider {...settings}> */}
+					<div className='col'>
+						<div className='reference-item'>
+							<div className='reference-img'>
+								<img src={ref1} alt='Bosch' />
+							</div>
+							<div className='reference-text'>
+								<p className='reference-text-title'>
+									Robert Bosch
+								</p>
+								<p className='reference-text-description'>
+									{t("reference.bosch")}
+								</p>
 							</div>
 						</div>
-						<div className='col'>
-							<div className='reference-item'>
-								<div className='reference-img'>
-									<img src={ref2} alt='The Body Shop' />
-								</div>
-								<div className='reference-text'>
-									<p className='reference-text-title'>
-										The Body Shop
-									</p>
-									<p className='reference-text-description'>
-										{t("reference.body")}
-									</p>
-								</div>
+					</div>
+					<div className='col'>
+						<div className='reference-item'>
+							<div className='reference-img'>
+								<img src={ref2} alt='The Body Shop' />
+							</div>
+							<div className='reference-text'>
+								<p className='reference-text-title'>
+									The Body Shop
+								</p>
+								<p className='reference-text-description'>
+									{t("reference.body")}
+								</p>
 							</div>
 						</div>
-						<div className='col'>
-							<div className='reference-item'>
-								<div className='reference-img'>
-									<img src={ref3} alt='Lapland Hotels' />
-								</div>
-								<div className='reference-text'>
-									<p className='reference-text-title'>
-										Lapland Hotels
-									</p>
-									<p className='reference-text-description'>
-										{t("reference.lapland")}
-									</p>
-								</div>
+					</div>
+					<div className='col'>
+						<div className='reference-item'>
+							<div className='reference-img'>
+								<img src={ref3} alt='Lapland Hotels' />
+							</div>
+							<div className='reference-text'>
+								<p className='reference-text-title'>
+									Lapland Hotels
+								</p>
+								<p className='reference-text-description'>
+									{t("reference.lapland")}
+								</p>
 							</div>
 						</div>
-						<div className='col'>
-							<div className='reference-item'>
-								<div className='reference-img'>
-									<img src={ref4} alt='IKEA' />
-								</div>
-								<div className='reference-text'>
-									<p className='reference-text-title'>IKEA</p>
-									<p className='reference-text-description'>
-										{t("reference.ikea")}
-									</p>
-								</div>
+					</div>
+					<div className='col'>
+						<div className='reference-item'>
+							<div className='reference-img'>
+								<img src={ref4} alt='IKEA' />
+							</div>
+							<div className='reference-text'>
+								<p className='reference-text-title'>IKEA</p>
+								<p className='reference-text-description'>
+									{t("reference.ikea")}
+								</p>
 							</div>
 						</div>
-					</Slider>
+					</div>
+					{/* </Slider> */}
 				</div>
 			</div>
 		</div>
