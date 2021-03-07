42549a432473a25cfda8342db978421d14c82187
thang241190 <thangnguyen24111990@gmail.com>
2021-03-07 15:35:04 +0200

    Revert to old code

--- src/components/references/ReferencesComponent.tsx
+++ src/components/references/ReferencesComponent.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import Slider, { Settings } from "react-slick"
+import Slider from "react-slick"
 import { useTranslation } from "react-i18next"
 import "../../i18n"
 
--- src/react-slick.d.ts
+++
@@ -1,4 +0,0 @@
-import Slider, { Settings } from "react-slick"
-declare module "react-slick" {
-	export { Slider, Settings }
-}
